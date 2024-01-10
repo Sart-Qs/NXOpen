eafb345cd16114919f3e0824c5a92be6a1021f6b
Sart-Qs <146476356+Sart-Qs@users.noreply.github.com>
2024-01-10 09:52:12 +0900

    Add files via upload

---
+++ newFormProject/project_UG.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <uf.h>
+#include <uf_modl.h>
+#include <uf_curve.h>
+#include <uf_csys.h>
+#include <uf_modl.h>
+#include <stdio.h>
+#include <uf.h>
+#include <uf_modl.h>
+#include <uf_curve.h>
+#include "form.hpp"
+
+
+int main() 
+{
+	form form;
+	form.Show();
+}
